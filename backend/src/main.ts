22783c6dc5e041c0d1eff939f7602b1e05cb135a
Mathyan <matijan.cvitanovic@outlook.com>
2024-09-08 14:25:35 +0200

    Going back to no ssl in app and will be using proxy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,22 +9,8 @@ async function bootstrap() {
   app.use(cookieParser());
   app.setGlobalPrefix('api');
 
-  const configService = app.get(ConfigService);
-  const isProd = configService.get<string>('NODE_ENV') === 'production';
-
-  if (isProd) {
-    const httpsOptions = {
-      key: readFileSync(configService.get<string>('SSL_KEY_PATH')),
-      cert: readFileSync(configService.get<string>('SSL_CERT_PATH')),
-    };
-
-    const httpsApp = await NestFactory.create(AppModule, { httpsOptions });
-    await httpsApp.listen(3000, '0.0.0.0');
-    console.log(`Application is running on HTTPS: ${await httpsApp.getUrl()}`);
-  } else {
-    await app.listen(3000, '0.0.0.0');
-    console.log(`Application is running on HTTP: ${await app.getUrl()}`);
-  }
+  await app.listen(3000, '0.0.0.0');
+  console.log(`Application is running on HTTP: ${await app.getUrl()}`);
 }
 
 bootstrap();
